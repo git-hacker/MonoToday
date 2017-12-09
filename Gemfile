af5715e2c4e0a9e0e8b6be812d55497ae2743b05
TylerChang <TylerChangMore@gmail.com>
2017-12-09 21:49:57 +0800

    Generate and set cookie on first visit

--- Gemfile
+++ Gemfile
@@ -35,6 +35,9 @@ gem 'turbolinks', '~> 5'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
+# Use awesome_print for format print log
+gem 'awesome_print'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,25 @@
 class TasksController < ApplicationController
   layout 'mono_today'
+  before_action :find_and_init_user_id
 
   def quick
+    ap @user_id
+  end
+
+  def grass_catcher
+    ap @user_id
+  end
+
+  private
+
+  def find_and_init_user_id
+    @user_id = cookies['user_id'] || ''
+    if @user_id.strip.empty?
+      @user_id = SecureRandom.uuid
+      cookies[:user_id] = {
+        value: @user_id,
+        expires: 1.year.from_now,
+      }
+    end
   end
 end
