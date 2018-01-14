6a3e3385105b75468e06bea64bca3a7d77f5e07f
TylerChang <TylerChangMore@gmail.com>
2018-01-14 17:44:56 +0800

    Completed stop mono and notification

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -13,13 +13,17 @@ document.addEventListener("turbolinks:load", function() {
     thickness: 6,
     reverse: true,
     fill: {
-      color: "#0096FF"
+      color: "#0064ff"
     },
     animation: false
   });
 
   $(".btn-start").on("click", function() {
     startDownCount();
+  });
+
+  $(".btn-stop").on("click", function() {
+    stopDownCount();
   });
 
   function startDownCount() {
@@ -32,17 +36,22 @@ document.addEventListener("turbolinks:load", function() {
     totalDiffMS = endTime.diff(startTime);
 
     intervalHandle = setInterval(function() {
-      var surplusDiffMS = endTime.diff(moment());
+      var surplusDiffMS = Math.ceil(endTime.diff(moment()) / 1000) * 1000;
       if (surplusDiffMS > 0) {
-        $(".time").text(moment(surplusDiffMS).utc().format("mm:ss"));
+        $(".time").text(
+          moment(surplusDiffMS)
+            .utc()
+            .format("mm:ss")
+        );
         $("#circle").circleProgress({ value: surplusDiffMS / totalDiffMS });
       } else {
-        onDownCountEnd();
+        stopDownCount();
+        notify();
       }
     }, 1000);
   }
 
-  function onDownCountEnd() {
+  function stopDownCount() {
     $(".tasks-content").show();
     $("footer").show();
     $(".clock-wrapper").hide();
@@ -52,5 +61,25 @@ document.addEventListener("turbolinks:load", function() {
       value: 1
     });
     clearInterval(intervalHandle);
+  }
+
+  function notify() {
+    promiseNotificationPermission(function() {
+      var notification = new Notification("恭喜您，完成了一个单核时段。接下来进入全境模式。");
+    });
+  }
+
+  function promiseNotificationPermission(callback) {
+    if (!("Notification" in window)) {
+      return false;
+    } else if (Notification.permission === "granted") {
+      callback();
+    } else if (Notification.permission !== "denied") {
+      Notification.requestPermission(function(permission) {
+        if (permission === "granted") {
+          callback();
+        }
+      });
+    }
   }
 });
